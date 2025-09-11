3213a18d6f2a53435717a8b41c5ac9bf8af0c654
YarleiWillersDev <yarlei.willers@gmail.com>
2025-09-10 21:59:51 -0300

    chore: projeto inicial Spring Boot controle de estoque

---
+++ src/main/java/br/com/barghesla/bar_estoque/ControleDeEstoqueDoBarApplication.java
@@ -0,0 +1,13 @@
+package br.com.barghesla.bar_estoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeEstoqueDoBarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDeEstoqueDoBarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/barghesla/bar_estoque/ControleDeEstoqueDoBarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.barghesla.bar_estoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDeEstoqueDoBarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
