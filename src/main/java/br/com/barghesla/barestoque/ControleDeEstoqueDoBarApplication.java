3da0b75e1cfc90e4e195c08d291f33f2454d0b94
YarleiWillersDev <yarlei.willers@gmail.com>
2025-09-10 23:06:33 -0300

    Configuração de perfis dev e prod no application.properties com Flyway e datasource Oracle

--- src/main/java/br/com/barghesla/barestoque/ControleDeEstoqueDoBarApplication.java
+++ src/main/java/br/com/barghesla/barestoque/ControleDeEstoqueDoBarApplication.java
@@ -1,4 +1,4 @@
-package br.com.barghesla.bar_estoque;
+package br.com.barghesla.barestoque;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
