2491c27d568af103e9b3071279a4a45011721773
YarleiWillersDev <yarlei.willers@gmail.com>
2025-10-28 22:51:55 -0300

    feat(docs): Adiciona documentação completa da API com SpringDoc/OpenAPI
    Implementa a documentação detalhada em todos os endpoints do ProdutoController, utilizando as anotações do Swagger/OpenAPI.
    
    - Adiciona @Operation e @ApiResponse em todos os métodos para descrever suas funcionalidades e possíveis retornos (2xx, 4xx).
    - Utiliza @Parameter para documentar os parâmetros de path e de query.
    - Refatora a declaração de @ApiResponse para o formato de anotação repetível, tornando o código mais limpo.
    - Adiciona uma configuração global do Swagger para incluir título, descrição e informações de contato da API.
    - Implementa um DTO padrão (ApiErrorResponse) para respostas de erro consistentes.
    - Configura o Swagger para ser desabilitado em ambiente de produção (@Profile("!prod")).

--- src/main/java/br/com/barghesla/barestoque/controller/produto/ProdutoController.java
+++ src/main/java/br/com/barghesla/barestoque/controller/produto/ProdutoController.java
@@ -19,10 +19,17 @@ import br.com.barghesla.barestoque.dto.produto.ProdutoRequest;
 import br.com.barghesla.barestoque.dto.produto.ProdutoResponse;
 import br.com.barghesla.barestoque.dto.produto.ProdutoUpdateStatusRequest;
 import br.com.barghesla.barestoque.service.produto.ProdutoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/produtos")
+@Tag(name = "Produtos", description = "Operações relacionadas aos pordutos do estoque")
 public class ProdutoController {
 
     private final ProdutoService produtoService;
@@ -33,43 +40,92 @@ public class ProdutoController {
     }
 
     @PostMapping
+    @Operation(summary = "Criar um novo produto", description = "Cria um novo produto cadastrando-o na base de dados")
+    @ApiResponse(responseCode = "201", description = "Produto criado com sucesso", 
+                 content = @Content(mediaType = "application/json", schema = @Schema(implementation = ProdutoResponse.class)))
+    @ApiResponse(responseCode = "400", description = "Dados inválidos fornecidos", content = @Content)
+    @ApiResponse(responseCode = "403", description = "Acesso negado. Requer perfil de 'GERENTE'.", content = @Content)
+
     public ResponseEntity<ProdutoResponse> cadastrarNovoProduto(@Valid @RequestBody ProdutoRequest produtoRequest) {
         ProdutoResponse novoProduto = produtoService.criar(produtoRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(novoProduto);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<ProdutoResponse> atualizarProdutoExistente(@Valid @RequestBody ProdutoRequest produtoRequest, @PathVariable Long id) {
+    @Operation(summary = "Atuliazar um produto existente", description = "Atualiza um produto existente alterando-o na base de dados")
+    @ApiResponse(responseCode = "200", description = "Produto atualizado com sucesso", 
+                 content = @Content(mediaType = "application/json", schema = @Schema(implementation = ProdutoResponse.class)))
+    @ApiResponse(responseCode = "400", description = "Dados inválidos fornecidos", content = @Content)
+    @ApiResponse(responseCode = "403", description = "Acesso negado. Requer perfil de 'GERENTE'.", content = @Content)
+
+    public ResponseEntity<ProdutoResponse> atualizarProdutoExistente(
+        @Valid @RequestBody ProdutoRequest produtoRequest,
+        @Parameter(description = "ID do produto a ser atualizado") @PathVariable Long id) {
+        
         ProdutoResponse produtoAtualizado = produtoService.atualizar(id, produtoRequest);
         return ResponseEntity.status(HttpStatus.OK).body(produtoAtualizado);
     }
 
     @PatchMapping("/{id}/status")
-    public ResponseEntity<ProdutoResponse> atualizarStatus(@PathVariable Long id, @Valid @RequestBody ProdutoUpdateStatusRequest request) {
+    @Operation(summary = "Atualizar status do produto", description = "Atualiza o status de um produto existente salvando-o na base de dados")
+    @ApiResponse(responseCode = "200", description = "Status do produto atualizado com sucesso",
+                 content = @Content(mediaType = "application/json", schema = @Schema(implementation = ProdutoResponse.class)))
+    @ApiResponse(responseCode = "400", description = "Dados inválidos fornecidos", content = @Content)
+    @ApiResponse(responseCode = "403", description = "Acesso negado. Requer perfil de 'GERENTE'.", content = @Content)
+
+    public ResponseEntity<ProdutoResponse> atualizarStatus(@PathVariable Long id,
+            @Valid @RequestBody ProdutoUpdateStatusRequest request) {
         ProdutoResponse produtoComStatusAtualizado = produtoService.atualizarStatus(id, request.status());
         return ResponseEntity.status(HttpStatus.OK).body(produtoComStatusAtualizado);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ProdutoResponse> buscarPorId(@PathVariable Long id) {
+    @Operation(summary = "Buscar produto pelo ID", description = "Busca o produto referênciado pelo ID passado pelo request")
+    @ApiResponse(responseCode = "200", description = "Produto encontrado com sucesso",
+                 content = @Content(mediaType = "application/json", schema = @Schema(implementation = ProdutoResponse.class)))
+    @ApiResponse(responseCode = "400", description = "Dados inválidos fornecidos", content = @Content)
+    @ApiResponse(responseCode = "403", description = "Acesso negado. Requer perfil de 'GERENTE'.", content = @Content)
+
+    public ResponseEntity<ProdutoResponse> buscarPorId(@Parameter(description = "ID do produto encontrado") @PathVariable Long id) {
         ProdutoResponse produtoEncontradoPeloId = produtoService.buscarPorId(id);
         return ResponseEntity.status(HttpStatus.OK).body(produtoEncontradoPeloId);
     }
 
     @GetMapping("/buscar")
-    public ResponseEntity<List<ProdutoResponse>> buscarPorNome(@RequestParam(name = "nome") String nome) {
+    @Operation(summary = "Buscar produto pelo nome", description = "Busca o produto referênciado pelo nome passado pelo request")
+    @ApiResponse(responseCode = "200", description = "Produto encontrado com sucesso",
+                 content = @Content(mediaType = "application/json", schema = @Schema(implementation = ProdutoResponse.class)))
+    @ApiResponse(responseCode = "400", description = "Dados inválidos fornecidos", content = @Content)
+    @ApiResponse(responseCode = "403", description = "Acesso negado. Requer perfil de 'GERENTE'.", content = @Content)
+
+    public ResponseEntity<List<ProdutoResponse>> buscarPorNome(
+        @Parameter(description = "Nome ou parte do nome do produto a ser pesquisado")
+        @RequestParam(name = "nome") String nome) {
+            
         List<ProdutoResponse> produtosEncotradosPorNome = produtoService.buscarPorNome(nome);
         return ResponseEntity.status(HttpStatus.OK).body(produtosEncotradosPorNome);
     }
 
     @GetMapping(params = "categoriaId")
-    public ResponseEntity<List<ProdutoResponse>> buscarPorCategoria(@RequestParam Long categoriaId) {
+    @Operation(summary = "Buscar produto pelo ID da categoria", description = "Busca o produto referênciado pelo ID da categoria passado pelo request")
+    @ApiResponse(responseCode = "200", description = "Produto encontrado com sucesso",
+                 content = @Content(mediaType = "application/json", schema = @Schema(implementation = ProdutoResponse.class)))
+    @ApiResponse(responseCode = "400", description = "Dados inválidos fornecidos", content = @Content)
+    @ApiResponse(responseCode = "403", description = "Acesso negado. Requer perfil de 'GERENTE'.", content = @Content)
+
+    public ResponseEntity<List<ProdutoResponse>> buscarPorCategoria(@Parameter(description = "ID da categoria referenciada")@RequestParam Long categoriaId) {
         List<ProdutoResponse> produtosEncontradosPelaCategoria = produtoService.buscarPorCategoria(categoriaId);
         return ResponseEntity.status(HttpStatus.OK).body(produtosEncontradosPelaCategoria);
     }
 
     @GetMapping
-    public ResponseEntity<List<ProdutoResponse>> listarTodos () {
+    @Operation(summary = "Buscar todos produtos cadastrados", description = "Busca todos os produtos que estão cadastrados na base de dados")
+    @ApiResponse(responseCode = "200", description = "Produtos encontrados com sucesso",
+                 content = @Content(mediaType = "application/json", schema = @Schema(implementation = ProdutoResponse.class)))
+    @ApiResponse(responseCode = "400", description = "Dados inválidos fornecidos", content = @Content)
+    @ApiResponse(responseCode = "403", description = "Acesso negado. Requer perfil de 'GERENTE'.", content = @Content)
+
+    public ResponseEntity<List<ProdutoResponse>> listarTodos() {
         List<ProdutoResponse> listaDeProdutos = produtoService.listarTodos();
         return ResponseEntity.status(HttpStatus.OK).body(listaDeProdutos);
     }
